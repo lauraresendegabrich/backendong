ba3486ab42072425177bcf7a9b93bf15e7eb60f9
Laura Resende Gabrich <lauragabrich2@gmail.com>
2024-12-04 20:20:52 -0300

    alterando conexao banco de dados

--- conexao_banco.js
+++ conexao_banco.js
@@ -4,10 +4,11 @@ const mysql = require('mysql2');
 
 // Configuração da conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'll300703',
-    database: 'bdong'
+    host: 'bywkfdhs9ujddowzpp0q-mysql.services.clever-cloud.com',
+    user: 'u4tfgiwlvubnpb71',
+    password: 'Bq22tuCkgIkYoqovyw2J',
+    database: 'bywkfdhs9ujddowzpp0q',
+    port: 3306
 });
 
 // Verifica se a conexão com o banco foi bem-sucedida
