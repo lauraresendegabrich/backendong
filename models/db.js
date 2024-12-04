e69af6d4a5a25f0f27d8e6e6bbed520116fb41f5
Laura Resende Gabrich <lauragabrich2@gmail.com>
2024-12-04 20:50:02 -0300

    musando a conexao do db

--- models/db.js
+++ models/db.js
@@ -1,9 +1,11 @@
 const Sequelize = require("sequelize");
 
 //Configuração do Banco de Dados MySQL
-const sequelize = new Sequelize('bdong', 'root', '1308', {
-    host: "localhost",
-    dialect: 'mysql'
+const sequelize = new Sequelize('bywkfdhs9ujddowzpp0q', 'u4tfgiwlvubnpb71', 'Bq22tuCkgIkYoqovyw2J', {
+    host: 'bywkfdhs9ujddowzpp0q-mysql.services.clever-cloud.com',
+    dialect: 'mysql',
+    port: 3306, // Certifique-se de que a porta está correta
+    logging: false // Opcional: desativa logs SQL no console
 });
 
 module.exports = {
