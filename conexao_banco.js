e98e6483706ef54b12b03c2e558d60fcb2fbf28f
Laura Resende Gabrich <lauragabrich2@gmail.com>
2024-12-04 21:17:15 -0300

    voltando banco local para testar

--- conexao_banco.js
+++ conexao_banco.js
@@ -4,11 +4,10 @@ const mysql = require('mysql2');
 
 // Configuração da conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'bywkfdhs9ujddowzpp0q-mysql.services.clever-cloud.com',
-    user: 'u4tfgiwlvubnpb71',
-    password: 'Bq22tuCkgIkYoqovyw2J',
-    database: 'bywkfdhs9ujddowzpp0q',
-    port: 3306
+    host: 'localhost',
+    user: 'root',
+    password: 'll300703',
+    database: 'bdong',
 });
 
 // Verifica se a conexão com o banco foi bem-sucedida
