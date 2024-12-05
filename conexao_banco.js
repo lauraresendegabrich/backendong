a3f761262255a81fcf4839358429fdd16e5eae5c
Laura Resende Gabrich <lauragabrich2@gmail.com>
2024-12-05 14:14:00 -0300

    alterando para poll

--- conexao_banco.js
+++ conexao_banco.js
@@ -1,23 +1,25 @@
 // conexao_banco.js
 const mysql = require('mysql2');
 
-
-// Configuração da conexão com o banco de dados
-const connection = mysql.createConnection({
+// Configuração do pool de conexões com o banco de dados
+const pool = mysql.createPool({
     host: 'bywkfdhs9ujddowzpp0q-mysql.services.clever-cloud.com',
     user: 'u4tfgiwlvubnpb71',
     password: 'Bq22tuCkgIkYoqovyw2J',
     database: 'bywkfdhs9ujddowzpp0q',
+    waitForConnections: true, // Aguarda conexões livres no pool quando atingido o limite
+    connectionLimit: 10,      // Número máximo de conexões simultâneas
+    queueLimit: 0             // Sem limite de requisições enfileiradas
 });
 
-// Verifica se a conexão com o banco foi bem-sucedida
-connection.connect((err) => {
+// Verifica se a conexão com o pool foi bem-sucedida
+pool.getConnection((err, connection) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados: ' + err.stack);
         return;
     }
-    console.log('Conexão bem-sucedida com o banco de dados');
+    console.log('Conexão bem-sucedida com o banco de dados (pool de conexões)');
+    connection.release(); // Libera a conexão de volta para o pool
 });
 
-module.exports = connection;
-
+module.exports = pool;
