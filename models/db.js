db326558c24b4246c16c09bba2431d847374ee0b
Laura Resende Gabrich <lauragabrich2@gmail.com>
2024-12-05 17:33:13 -0300

    alterando db sequelize host

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 
 //Configuração do Banco de Dados MySQL
 const sequelize = new Sequelize('bywkfdhs9ujddowzpp0q', 'u4tfgiwlvubnpb71', 'Bq22tuCkgIkYoqovyw2J', {
-    host: "localhost",
+    host: "bywkfdhs9ujddowzpp0q-mysql.services.clever-cloud.com",
     dialect: 'mysql'
 });
 
